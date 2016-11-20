29b41185535cc9b4bf30946fa245a244a5f14159
Brian Friesen <brianfriesen@quickenloans.com>
2016-11-19 22:14:39 -0500

    Add Instruction, Opcode, IOperand, and EmptyOperand

---
+++ garply/src/garplylib/Opcode.cs
@@ -0,0 +1,13 @@
+﻿namespace garply
+{
+    public enum Opcode : ushort
+    {
+        Nop,
+        Reserved1 = Instruction.MarkerByte1,
+        Reserved2 = Instruction.MarkerByte2,
+        Reserved3 = Instruction.MarkerByte3,
+        Reserved4 = Instruction.MarkerByte4,
+        Reserved5 = Instruction.MarkerByte5,
+        //ExampleOfExtendedOpecode = (Instruction.MarkerByte1 << 8) | 1,
+    }
+}
