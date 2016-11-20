ecc62e26dd00112bdd0ee7415ebc6e6877855ceb
Brian Friesen <brianfriesen@quickenloans.com>
2016-11-19 22:45:14 -0500

    Add Expression and ExpressionBuilder

---
+++ garply/src/garplylib/Expression.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace garply
+{
+    public struct Expression
+    {
+        private static readonly Instruction[] _emptyInstructions = new Instruction[0];
+
+        private Instruction[] _instructions;
+
+        public Expression(Instruction[] instructions)
+        {
+            _instructions = instructions;
+        }
+
+        public IReadOnlyList<Instruction> Instructions
+        {
+            get { return _instructions ?? (_instructions = _emptyInstructions); }
+        }
+
+        public static Expression Read(Stream stream)
+        {
+            using (var reader = new BinaryReader(stream, Encoding.UTF8, true))
+            {
+                var expression = new ExpressionBuilder();
+
+                var instructionCount = reader.ReadInt32();
+
+                for (int i = 0; i < instructionCount; i++)
+                {
+                    expression.Add(Instruction.Read(stream));
+                }
+
+                return expression.Build();
+            }
+        }
+
+        public void Write(Stream stream)
+        {
+            using (var writer = new BinaryWriter(stream, Encoding.UTF8, true))
+            {
+                writer.Write(_instructions.Length);
+            }
+
+            foreach (var instruction in _instructions)
+            {
+                instruction.Write(stream);
+            }
+        }
+    }
+}
---
+++ garply/src/garplylib/ExpressionBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace garply
+{
+    public class ExpressionBuilder : IEnumerable
+    {
+        private readonly List<Instruction> _instructions = new List<Instruction>();
+
+        public ExpressionBuilder Add(Instruction instruction)
+        {
+            _instructions.Add(instruction);
+            return this;
+        }
+
+        public Expression Build()
+        {
+            return new Expression(_instructions.ToArray());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_instructions).GetEnumerator();
+    }
+}
