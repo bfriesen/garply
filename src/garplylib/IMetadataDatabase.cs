339513f2361723594123e64b8185491576e5f20b
Brian Friesen <brian.friesen@gmail.com>
2016-11-20 05:23:23 -0500

    Add LoadType opcode

--- src/garplylib/IMetadataDatabase.cs
+++ src/garplylib/IMetadataDatabase.cs
@@ -3,6 +3,8 @@
     public interface IMetadataDatabase
     {
         Integer GetStringId(String value);
+        Integer GetTypeId(Type value);
         String LoadString(Integer id);
+        Type LoadType(Integer id);
     }
 }
--- src/garplylib/Type.cs
+++ src/garplylib/Type.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.IO;
 
 namespace garply
 {
-    public sealed class Type : ITyped
+    public sealed class Type : ITyped, IOperand
     {
         public static Type TypeType { get; } = new Type(Name.TypeName);
         public static Type TupleType { get; } = new Type(Name.TupleName);
@@ -28,6 +29,12 @@ namespace garply
 
         Type ITyped.Type => TypeType;
 
+        public void Write(BinaryWriter writer, IMetadataDatabase metadataDatabase)
+        {
+            var id = metadataDatabase.GetTypeId(this);
+            id.Write(writer, metadataDatabase);
+        }
+
         public bool Is(Type other)
         {
 #if UNSTABLE
@@ -36,6 +43,22 @@ namespace garply
             if (Equals(other)) return true;
             if (BaseType == null) return false;
             return BaseType.Is(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return unchecked((Name.GetHashCode() * 397) ^ (BaseType == null ? 0 : BaseType.GetHashCode()));
+        }
+
+        public override bool Equals(object obj)
+        {
+#if UNSTABLE
+            if (obj == null) throw new ArgumentNullException("other");
+#endif
+            if (ReferenceEquals(this, obj)) return true;
+            var other = obj as Type;
+            if (other == null) return false;
+            return Equals(other);
         }
 
         public bool Equals(Type other)
