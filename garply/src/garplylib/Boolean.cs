23400f4685eb59928d3e1ac3b347170959c08e01
Brian Friesen <brian.friesen@gmail.com>
2016-11-20 03:17:25 -0500

    Add a type system

---
+++ garply/src/garplylib/Boolean.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+
+namespace garply
+{
+    public class Boolean : ITyped, IOperand
+    {
+        public Boolean(bool value)
+        {
+            Value = value;
+        }
+
+        public Type Type => Type.BooleanType;
+
+        public bool Value { get; }
+
+        public void Write(Stream stream)
+        {
+            using (var writer = new BinaryWriter(stream, Encoding.UTF8, true))
+            {
+                writer.Write(Value);
+            }
+        }
+    }
+}
---
+++ garply/src/garplylib/Float.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+
+namespace garply
+{
+    public class Float : ITyped, IOperand
+    {
+        public Float(double value)
+        {
+            Value = value;
+        }
+
+        public Type Type => Type.FloatType;
+
+        public double Value { get; }
+
+        public void Write(Stream stream)
+        {
+            using (var writer = new BinaryWriter(stream, Encoding.UTF8, true))
+            {
+                writer.Write(Value);
+            }
+        }
+    }
+}
