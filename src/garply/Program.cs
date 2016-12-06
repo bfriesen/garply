ecd312beefb5dc10fd384d12679d96ab2133bb41
Brian Friesen <brian.friesen@gmail.com>
2016-12-06 18:18:47 -0500

    Move repl to separate file

--- src/garply/Program.cs
+++ src/garply/Program.cs
@@ -1,69 +1,16 @@
-﻿using System;
-
-namespace Garply
+﻿namespace Garply
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            if (args == null || args.Length == 0) // REPL
+            if (args == null || args.Length == 0)
             {
-                Repl();
+                Repl.Start();
             }
             else
             {
 
-            }
-        }
-
-        private static void Repl()
-        {
-            var executionContext = new ExecutionContext();
-            var parser = new GarplyParser(executionContext);
-
-            Console.Clear();
-
-            while (true)
-            {
-                Console.Write("garply> ");
-                string line;
-                switch (line = Console.ReadLine().Trim())
-                {
-                    case "": continue;
-                    case ":q": return;
-                    case ":c": Console.Clear(); continue;
-                    case ":d":
-                        Console.WriteLine();
-                        Console.WriteLine(Heap.Dump);
-                        Console.WriteLine();
-                        continue;
-                }
-                var parseResult = parser.ParseLine(line);
-                switch (parseResult.Type)
-                {
-                    case Types.Expression:
-                    {
-                        var value = Heap.GetExpression((int)parseResult.Raw).Evaluate(executionContext);
-                        if (value.Type == Types.Error)
-                        {
-                            var error = executionContext.GetError();
-                            Console.WriteLine(error.ToString());
-                            error.RemoveRef();
-                        }
-                        else Console.WriteLine(value.ToString());
-                        value.RemoveRef();
-                        parseResult.RemoveRef();
-                        break;
-                    }
-                    case Types.Error:
-                    {
-                        var error = executionContext.GetError();
-                        Console.WriteLine(error.ToString());
-                        error.RemoveRef();
-                        break;
-                    }
-                    default: break;
-                }
             }
         }
     }
